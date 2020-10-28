86ce7206c70294edab6784e84da0a06bd23c4be2
mpmkp2020 <madhukarhbti@gmail.com>
2020-10-28 17:46:22 +0530

    Update tasks.py

--- tools/tasks.py
+++ tools/tasks.py
@@ -287,7 +287,8 @@ def manylinux(ctx, vs, upload=False, pythons=manylinux_pys):
         else:
             run(base_cmd +  " quay.io/pypa/manylinux1_x86_64 /io/build_pyzmqs.sh")
     if upload:
-        run(['ls', '-l', os.path.join(manylinux, 'wheelhouse', '*')])
+        run(['pwd'])
+        run(['ls', '-l'])
         run(['ls', '-l', 'wheelhouse/*'])
         py = make_env(default_py, 'twine')
         run(['twine', 'upload', os.path.join(manylinux, 'wheelhouse', '*')])
